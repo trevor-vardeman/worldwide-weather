f9c6c2000ffb9082716c9feb75243e1dcdeb12a6
Trevor Vardeman <trevortx@gmail.com>
2022-03-25 19:45:23 -0700

    api key hidden and console logging current weather successfully

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => console.log("test"))
+const key = config.API_KEY
 
-// get user's IP
+// document.addEventListener("DOMContentLoaded", getIP)
+
+// // get user's IP
 // function getIP() {
-//   fetch("https://www.cloudflare.com/cdn-cgi/trace")
-//     .then(resp => resp.text())
+//   fetch("https://api.ipify.org/?format=json")
+//     .then(res => res.json())
 //     .then(data => console.log(data))
 // }
 
@@ -21,5 +23,32 @@ function executeSearch(e) {
 
 // fetches current weather at location of user's search
 function getCurrentWeather(searchTerm) {
-  console.log(searchTerm)
+  fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${searchTerm}&aqi=no`)
+    .then(response => response.json())
+    .then(response => checkForValidCity(response))
+    .catch(err => alert(err))
+}
+
+// checks for valid city or invalid search term
+function checkForValidCity(city) {
+  if (city.current != null) {
+    cardCreator(city)
+  } else alert(`${city.error.message} Please search again.`)
+}
+
+function currentWeather(weather) {
+  // set variables
+  const currentCondition = weather.current.condition.text
+  const currentTempF = weather.current.temp_f
+  const currentTempC = weather.current.temp_c
+  const feelsLikeF = weather.current.feelslike_f
+  const feelsLikeC = weather.current.feelslike_c
+  const windMPH = weather.current.wind_mph
+  const windKPH = weather.current.wind_kph
+  const windDirection = weather.current.wind_dir
+  console.log(weather)
+}
+
+function futureForecast(weather) {
+  
 }
