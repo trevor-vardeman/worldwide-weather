cc5bfee2422a3643d1180ec64525ca3538d4099b
Trevor Vardeman <trevortx@gmail.com>
2022-03-27 09:20:18 -0700

    creating html elements and setting attributes

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function executeSearch(e) {
 
 // fetches current weather at location of user's search
 function getCurrentWeather(searchTerm) {
-  fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${searchTerm}&aqi=no`)
+  fetch(`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${searchTerm}&days=3&aqi=no&alerts=no`)
     .then(response => response.json())
     .then(response => checkForValidCity(response))
     .catch(err => alert(err))
@@ -31,13 +31,13 @@ function getCurrentWeather(searchTerm) {
 
 // checks for valid city or invalid search term
 function checkForValidCity(city) {
-  if (city.current != null) {
-    cardCreator(city)
+  if (city.location != null) {
+    createCards(city)
   } else alert(`${city.error.message} Please search again.`)
 }
 
-function currentWeather(weather) {
-  // set variables
+function createCards(weather) {
+  // set current weather variables
   const currentCondition = weather.current.condition.text
   const currentTempF = weather.current.temp_f
   const currentTempC = weather.current.temp_c
@@ -46,9 +46,68 @@ function currentWeather(weather) {
   const windMPH = weather.current.wind_mph
   const windKPH = weather.current.wind_kph
   const windDirection = weather.current.wind_dir
+
+  // set future forecast variables
+
+  // create HTML elements & add attributes
+  const cardColumn = document.createElement("div")
+  cardColumn.setAttribute("class", "card mb-3")
+  cardColumn.setAttribute("style", "max-width: 540px;")
+
+  const cardRow = document.createElement("div")
+  cardRow.setAttribute("class", "row g-0")
+
+  const imgColumn = document.createElement("div")
+  imgColumn.setAttribute("class", "col-md-4")
+
+  const newImg = document.createElement("img")
+  newImg.setAttribute("src", "https://via.placeholder.com/150")
+  newImg.setAttribute("class", "img-fluid rounded-start")
+  newImg.setAttribute("alt", "text here")
+
+  const textColumn = document.createElement("div")
+  textColumn.setAttribute("class", "col-md-8")
+
+  const cardHeader = document.createElement("div")
+  cardHeader.setAttribute("class", "card-header")
+  cardHeader.innerText = "Today"
+
+  const cardBody = document.createElement("div")
+  cardBody.setAttribute("class", "card-body")
+
+  const cardTitle = document.createElement("h5")
+  cardTitle.setAttribute("class", "card-title")
+  cardTitle.innerText = `${currentCondition}`
+
+  const cardText = document.createElement("p")
+  cardText.setAttribute("class", "card-text")
+
+  const listGroup = document.createElement("ul")
+  listGroup.setAttribute("class", "list-group")
+
+  const listItem = document.createElement("li")
+  listItem.setAttribute("class", "list-group-item")
+
+  cardColumn.setAttribute("class", "card mb-3")
+  cardColumn.setAttribute("style", "max-width: 540px;")
+  cardRow.setAttribute("class", "row g-0")
+
+  const cardGroup = document.querySelector(".card-group")
+  cardGroup.append(cardColumn)
+  cardColumn.append(cardRow)
+  cardRow.append(imgColumn)
+  imgColumn.append(newImg)
+  cardRow.append(textColumn)
+  textColumn.append(cardHeader)
+  textColumn.append(cardBody)
+  cardBody.append(cardTitle)
+  cardBody.append(cardText)
+  cardText.append(listGroup)
+  listGroup.append(listItem)
+  
   console.log(weather)
 }
 
-function futureForecast(weather) {
-  
-}
+// function futureForecast(weather) {
+//   const highTempF = weather.forecast.
+// }
